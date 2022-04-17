ab62da21d02bf2e573cb280dda96891a0849b6b2
stevenEno <steven.e.eno@gmail.com>
2022-04-16 21:01:03 -0700

    High/low temp comparison viz and world earthquake viz

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import date, datetime
 
 import matplotlib.pyplot as plt
 
@@ -8,19 +9,22 @@ with open(filename) as f:
     header_row = next(reader)
     
     # Get high temperatures from this file.
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
 # Plot the high temperatures.
 plt.style.use('seaborn')
-fix, ax = plt.subplots()
-ax.plot(highs, c='red')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red')
 
 # Format plot.
-ax.set_title("Daily high temperatures, 2018", fontsize = 24)
+ax.set_title("Daily high temperatures - 2018", fontsize = 24)
 ax.set_xlabel('', fontsize = 16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize = 16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
